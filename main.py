508e779391ce63ac61b758f3a571ecc3c1400091
leonumerouno <elnumerouno-@outlook.com>
2024-07-11 16:33:56 +0800

    1

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ session = requests.Session()
 cookies = {'token': os.environ['TOKEN']}
 session.cookies.update(cookies)
 
-# 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFeHBpcmVBdCI6MTcyMDc2ODk3OCwiVW5pb25JRCI6Im9Jalp4NlV5akZFRnQ0cVlpcjhnbFdXdEs4M1UifQ.JRg_W3Wh__WFODSXIRhoHzomZF7n5nEtDU_rZaK61JU'
-# url = "https://swjtu-grade-push.tpam.top/notions"
-
 response = session.get(os.environ['GETURL'])
 
 resp = json.loads(response.text)
@@ -16,8 +13,6 @@ resp = json.loads(response.text)
 posturl = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id"
 
 headers = {'Authorization': os.environ['HEADERS']}
-
-# Bearer t-g1047bfxUYK4E7BAEV73D2P4A5WBFNWLVQ367CPL
 
 if resp['msg'] == "ok":
     datas = {
