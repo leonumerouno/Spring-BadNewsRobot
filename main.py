97146b96fffcc155a711a9cecd90d0209c68b1d6
leonumerouno <elnumerouno-@outlook.com>
2024-07-12 09:58:07 +0800

    1

--- main.py
+++ main.py
@@ -2,35 +2,34 @@ import requests
 import json
 import os
 
+#获取tenant_access_token
+token_url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
+
+datas = {
+    "app_id": os.environ['APPID'],
+    "app_secret": os.environ['APPSECRET']
+}
+
+resp = requests.post(token_url,data=datas)
+auth = "Bearer " + json.loads(resp.text)['tenant_access_token']
+
+#访问喷泉
 session = requests.Session()
 cookies = {'token': os.environ['TOKEN']}
 session.cookies.update(cookies)
 
-response = session.get(os.environ['GETURL'])
-
-resp = json.loads(response.text)
-
-posturl = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id"
-
-headers = {'Authorization': os.environ['HEADERS']}
-
-if resp['msg'] == "ok":
+try:
+    response = session.get(os.environ['GETURL'],timeout=10)
+except requests.exceptions.RequestException as e:
+    #如果超时，飞书机器人发消息
+    posturl = "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id"
+    headers = {'Authorization': auth}
     datas = {
-        "receive_id": os.environ['RECEIVEID'],
-        "content": "{\"text\":\"server running\"}",
-        "msg_type": "text"
-    }
-    response = session.post(url=posturl,data=datas,headers = headers)
-    print(response.text)
-
-else:
-    datas = {
-        "receive_id": os.environ['RECEIVEID'],
-        "content": "{\"text\":\"server G\"}",
-        "msg_type": "text"
-    }
+            "receive_id": os.environ['RECEIVEID'],
+            "content": "{\"text\":\"<at user_id=\\\\" + os.environ['USERID'] + "\\\">马熙翔</at> 服务器停止运行\"}",
+            "msg_type": "text"
+        }
     response = session.post(url=posturl, data=datas, headers=headers)
     print(response.text)
-
 
 
